dee3c6974196c4dd3c3d45da056cf7f08bce7531
trav <travv55055@gmail.com>
2026-08-13 21:14:41 +0200

    Aligned the Polish l18n file to the base English

--- Data Files/MWSE/mods/TamrielData/i18n/pol.lua
+++ Data Files/MWSE/mods/TamrielData/i18n/pol.lua
@@ -1,5 +1,6 @@
 return {
 	-- Main Text
+	["main.imgaBoots"] = "Imga nie mogą nosić butów.",
 	["main.imgaShoes"] = "Imga nie mogą nosić butów.",
 	["main.imgaHelm"] = "Samce Imga nie mogą nosić hełmów.", -- Aly: "samiec" is ok, I guess, it's a word used towards male animals which probably fit in case of beast(-ish?) race
 	["main.imgaHat"] = "Samce Imga nie mogą nosić nakryć głowy.",
@@ -102,13 +103,13 @@ return {
 	["magic.boundWarhammerDesc"] = "Przywołuje pomniejszą Daedrę zaklętą w postaci magicznego, niezwykle lekkiego, daedrycznego młota bojowego. Broń pojawia się automatycznie w dłoni rzucającego. Po upłynięciu czasu trwania efektu, młot znika, a postać ponownie dzierży swoją zwykłą broń.",
 	["magic.boundPauldrons"] = "Zaklęte Naramienniki",
 	["magic.boundPauldronsDesc"] = "Przywołuje pomniejszą Daedrę zaklętą w postaci magicznych, niezwykle lekkich, daedrycznych naramienników. Pancerz pojawia się automatycznie na ciele rzucającego. Po upłynięciu czasu trwania efektu, naramienniki znikają, a postać ponownie nosi swój zwykły pancerz.",
-	["magic.boundGreatsword"] = "Zaklęty Miecz Dwuręczny",
-	["magic.boundGreatswordDesc"] = "Przywołuje pomniejszą Daedrę zaklętą w postaci magicznego, niezwykle lekkiego, daedrycznego miecza dwuręcznego. Broń pojawia się automatycznie w dłoni rzucającego. Po upłynięciu czasu trwania efektu, młot znika, a postać ponownie dzierży swoją zwykłą broń.",
+	["magic.boundClaymore"] = "Zaklęty Miecz Dwuręczny",
+	["magic.boundClaymoreDesc"] = "Przywołuje pomniejszą Daedrę zaklętą w postaci magicznego, niezwykle lekkiego, daedrycznego miecza dwuręcznego. Broń pojawia się automatycznie w dłoni rzucającego. Po upłynięciu czasu trwania efektu, młot znika, a postać ponownie dzierży swoją zwykłą broń.",
 	["magic.boundThrowingKnives"] = "Zaklęte Noże do Rzucania",
 	["magic.boundThrowingKnivesDesc"] = "Przywołuje pomniejszą Daedrę zaklętą w postaci magicznych, niezwykle lekkich, daedrycznego noży do rzucania. Broń pojawia się automatycznie w dłoni rzucającego. Po upłynięciu czasu trwania efektu, noże znikają, a postać ponownie dzierży swoją zwykłą broń.",
 
 	["magic.interventionKyne"] = "Interwencja Kyne",
-	["magic.interventionKyneDesc"] = "Natychmiastowo przenosi rzucającego zaklęcie do najbliższej świątynii lub miejsca poświęconego norskiej bogini Kyne.",
+	["magic.interventionKyneDesc"] = "Natychmiastowo przenosi rzucającego zaklęcie do najbliższej świątyni lub miejsca poświęconego norskiej bogini Kyne.",
 
 	["magic.miscPasswall"] = "Przeniknięcie Ściany", -- Aly: alternative could be "Przejście Ściany", (more direct) discuss with poles which one sounds better to them
 	["magic.miscPasswallDesc"] = "Pozwala przeniknąć przez stałe bariery w obszarach wewnętrznych do wolnej przestrzeni poza nimi. Efekt zawiedzie, jeśli miejsce docelowe jest wypełnione wodą, położone jest poza polem siłowym, zapieczętowaną bramą, lub znajduje się powyżej lub poniżej poziomu, na któym znajduje się rzucający zaklęcie.",
@@ -120,6 +121,7 @@ return {
 	["magic.miscDetectHumanoidDesc"] = "Rzucający zaklęcie może wykrywać na odległość obecność istot humanoidalnych. Zostaną one oznaczone na mapie specjalnym symbolem. Efekt obejmuje wszystkie postacie, a jego poziom to promień (w stopach) obszaru, który zostanie magicznie przeszukany.",
 	["magic.miscRadiantShield"] = "Tarcza Blasku",
 	["magic.miscRadiantShieldDesc"] = "Tworzy świetlistą osłonę dookoła celu. Stopień efektu dodawany jest do klasy pancerza. Odporność celu na szkodliwe efekty magiczne wzrasta. Dodatkowo, krótkotrwale oślepia przeciwników w zwarciu.",
+	["magic.miscRadiantShieldBlindness"] = "Oślepiający Blask",
 	["magic.miscWabbajack"] = "Łabadżak", -- Aly: yes, it's just directly transcribed to polish alphabet (that's what polish TESIV and TESV did), it's said exactly the same as english
 	["magic.miscWabbajackDesc"] = "Łabadżak!",
 	["magic.miscInsight"] = "Intuicja",
@@ -209,12 +211,17 @@ return {
 
 	-- MCM Text
 	["mcm.name"] = "Tamriel Data",
-	["mcm.preferences"] = "Preferencje",
+	["mcm.magic"] = "Magia",
+	["mcm.equipment"] = "Ekwipunek",
+	["mcm.fixes"] = "Poprawki",
+	["mcm.miscellaneous"] = "Pozostałe",
 	["mcm.version"] = "Tamriel Data MWSE-Lua v2.2",
 	["mcm.credits"] = "Autorzy:",
 	["mcm.Kynesifnar"] = "Kynesifnar - Oskryptowanie",
+	["mcm.EvilEye"] = "Evil Eye - Oskryptowanie",
 	["mcm.mort"] = "mort - Oskryptowanie",
 	["mcm.Rakanishu"] = "Rakanishu - Oskryptowanie",
+	["mcm.Stele"] = "Stele - Utrzymanie interakcji z innymi modami",
 	["mcm.chef"] = "chef - Zarządzanie TD_Addon-em",
 	["mcm.Cicero"] = "Cicero - Ikony",
 	["mcm.NullCascade"] = "NullCascade - Wsparcie MWSE",
@@ -236,7 +243,19 @@ return {
 	["mcm.passwallAlterationDescription"] = "Przypisuje Przeniknięcie ściany do szkoły Przemiany, zamiast szkoły Mistycyzmu.\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wył.\n\n",
 
 	["mcm.blinkIndicatorLabel"] = "Wskaźnik Migotania",
-	["mcm.blinkIndicatorDescription"] = "Podczas przygotowywania zaklęcia Migotania, zostanie wyświetlony wskaźnik celu teleportacji.\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
+	["mcm.blinkIndicatorDescription"] = "Podczas przygotowywania zaklęcia Migotania, zostanie wyświetlony wskaźnik celu teleportacji.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.argonianBloodMagicLabel"] = "Magia Krwi dla Argonian",
+	["mcm.argonianBloodMagicDescription"] = "Argonianie otrzymują Magię Krwi jako moc specjalną.\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.detectValuablesThresholdLabel"] = "Czułość Wykrycia Kosztowności",
+	["mcm.detectValuablesThresholdDescription"] = "Minimalna wartość przedmiotów wykrywalnych przez efekty Wykrycia Kosztowności.\n\nDomyślnie: 4000\n\n",
+
+	["mcm.prismaticLightSaturationLabel"] = "Nasycenie Pryzmatycznego Światła",
+	["mcm.prismaticLightSaturationDescription"] = "Wartość określająca intensywność kolorów Pryzmatycznego Światła.\n\nDomyślnie: 0.4\n\n",
+
+	["mcm.prismaticLightPeriodLabel"] = "Okres Pryzmatycznego Światła",
+	["mcm.prismaticLightPeriodDescription"] = "Czas (w sekundach) potrzebny Pryzmatycznemu Światłu do wyświetlenia pełnej palety swoich barw.\nWymaga ponownego wczytania gry.\n\nDomyślnie: 6\n\n",
 
 	["mcm.magickaExpandedLabel"] = "Nadpisanie Efektów Magicka Expanded",
 	["mcm.magickaExpandedDescription"] = "Podmienia, dla spójności, niektóre efekty dodane w Magicka Expanded na takie same zaimplementowane również w Tamriel Data, jak np. Wypędzenie Daedry czy Przywołanie Vermai" ..
@@ -251,32 +270,48 @@ return {
 
 	["mcm.weatherChangesLabel"] = "Nowe Efekty Pogodowe",
 	["mcm.weatherChangesDescription"] = "Dodaje nowe efekty pogodowe w niektórych regionach, jak np. opad zarodników w Lasach Othreleth, burze piaskowe w Shipal-Shin czy tropikalne burze w okolicach Morza Abeceańskiego. Wprowadza także poprawki pogodowe w pozostałych regionach, jak np. popielne burze w częściach kontynentalnych Morrowind niemające źródła na Czerwonej Górze."..
-										"\nMoże kolidować z innymi modami MWSE wpływającymi na efekty pogodowe, jak np. Watch the Skies.\nWymage ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
+										"\nMoże kolidować z innymi modami MWSE wpływającymi na efekty pogodowe, jak np. Watch the Skies.\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.hatsLabel"] = "Nakrycia Głów Jako Ubrania",
 	["mcm.hatsDescription"] = "Powoduje, że nakrycia głów z Tamriel Data funkcjonują jako ubrania, a nie pancerz."..
-											"\nMoże kolidować z innymi modam MWSE dodającymi nowe typy ubrań.\nWymage ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
+											"\nMoże kolidować z innymi modam MWSE dodającymi nowe typy ubrań.\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.creatureBehaviorsLabel"] = "Rozszerzenie Zachowania Stworzeń",
 	["mcm.creatureBehaviorsDescription"] = "Sprawia, że istoty zachowują się w bardziej interesujący lub mniej ograniczony przez możliwości silnika Morrowind, jak np. [Strident Runner] nie będą już niezdarnie wstawać na końcu każdej pętli animacji.".. --Aly: kinda half-assed without example part - for now
-											"\nWymage ponownego uruchomienia gry.\n\nDomyślnie: On\n\n",
+											"\nWymaga ponownego uruchomienia gry.\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.creatureSoundsLabel"] = "Dodatkowe Dźwięki Stworzeń",
+	["mcm.creatureSoundsDescription"] = "Pozwala stworzeniom z Tamriel Data na emisję dźwięków normalnie niewspieranych w grze, na przykład odgłosów ataku."..
+											"\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.animationFixLabel"] = "Poprawka Animacji Gracza dla Ras Dodanych w Tamriel Data",
 	["mcm.animationFixDescription"] = "Poprawia animacje dla odblokowanych przez mody grywalnych ras dodanych w Tamriel Data, takich jak Ohmes-raht czy Suthay.\nWymaga ponownego wczytania gry. Ogon może zniknąć do momentu ponownego wczytania gry, jeśli animacje z innych modów MWSE są w użyciu przez postać gracza." ..
 										"\nJeśli podmiana animacji dodająca kości ogona do base_anim.nif jest w użyciu, ta funkcja jest prawdopodobnie zbędna.\n\nDomyślnie: Wł.\n\n",
 
-	["mcm.restrictEquipmentLabel"] ="Ograniczenia Ekwipunku dla Ras Dodanych w Tamriel Data",
-	["mcm.restrictEquipmentDescription"] = "Powstrzymuje rasy dodane w Tamriel Data przed noszeniem pewnych części ekwipunku, gdy jest to fizycznie nieprawdopodobne lub technicznie problematyczne.\nWymaga ponownego wczytania gry.\n\nDotyczy ras oraz części ekwipunku:" ..
-											"\n- Imga nie mogą ubierać obuwia, samce Imga nie mogą ubierać hełmów.\n\nDomyślnie: Wł.\n\n",
+	["mcm.wereCreatureFixLabel"] = "Poprawka Wyglądu Nie-wilkołaków",
+	["mcm.wereCreatureFixDescription"] = "Ukrywa model wilkołaka, gdy gracz przemienia się w innego likantropa z Tamriel Data.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.fixVampireLabel"] = "Poprawka Wampirzych Głów",
 	["mcm.fixVampireDescription"] = "Powstrzymuje Całun Namiry od ukrywania głowy gracza, gdy jest ubrany, oraz pozwala wampirzym postaciom niezależnym używać unikatowych głów stworzonych specjalnie dla nich.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.restrictEquipmentLabel"] ="Ograniczenia Ekwipunku dla Ras Dodanych w Tamriel Data",
+	["mcm.restrictEquipmentDescription"] = "Powstrzymuje rasy dodane w Tamriel Data przed noszeniem pewnych części ekwipunku, gdy jest to fizycznie nieprawdopodobne lub technicznie problematyczne.\nWymaga ponownego wczytania gry.\n\nDotyczy ras oraz części ekwipunku:" ..
+											"\n- Imga nie mogą nosić obuwia, samce Imga nie mogą ubierać hełmów." ..
+											"\n- Tsaesci nie mogą nosić obuwia ani nagolenników." ..
+											"\n- Nagi nie mogą ubierać hełmów." ..
+											"\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.femaleArgoniansUseMaleEquipmentLabel"] = "Female Tamriel Data Argonians Use Male Equipment",
+	["mcm.femaleArgoniansUseMaleEquipmentDescription"] = "Kobiety ras argoniańskich pochodzących z Tamriel Data (na przykład Nagi) mogą nosić męskie ubrania i pancerz.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł., o ile opcja wyboru ubrań dla Argonian jest włączona w Morrowind Code Patch\n\n",
 
 	["mcm.itemSoundsLabel"] = "Poprawione Efekty Dźwiękowe Przedmiotów",
 	["mcm.itemSoundsDescription"] = "Niektóre przedmioty dodane w Tamriel Data, jak pasta wasabi czy perfumy, używają bardziej sensownych efektów dźwiękowych, gdy są użyte lub dodane do/usunięte z ekwipunku.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.travelPricesLabel"] = "Regulacja Kosztów Podróży",
-	["mcm.travelPricesDescription"] = "Zmienia koszty podróży do miejsc docelowych dodanych przez Tamriel Rebuilt i Project Tamriel w przypadku, gdy ceny obliczone przez grę są nierozsądne, jak np w przypadku sieci teleportacyjnej Gildii Magów.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
+	["mcm.travelPricesDescription"] = "Zmienia koszty podróży do miejsc docelowych dodanych przez Tamriel Rebuilt i Project Tamriel w przypadku, gdy ceny obliczone przez grę są nierozsądne, jak np. w przypadku sieci teleportacyjnej Gildii Magów.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
+
+	["mcm.reactCellItemsLabel"] = "Ochrona Przed Utratą Przedmiotów w Lokalizacjacch Specjalnych",
+	["mcm.reactCellItemsDescription"] = "Przedmioty pozostawione w obszarach dostępnych jedynie podczas zadań w Tamriel Rebuilt lub Project Tamriel zostaną przeniesione do wyznaczonych pojemników, z których można je odzyskać.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
 
 	["mcm.khajiitFormCharacterCreationLabel"] = "Określ Formy Khajiitów Podczas Tworzenia Postaci",
 	["mcm.khajiitFormCharacterCreationDescription"] = "Nazywa formy Khajiitów w stosownych menu tworzenia postaci, zamiast kolektywnej nazwy \"Khajiit\". Dodatkowy mod, taki jak All Races and Classes Unlocked jest wymagany, aby odblokować wspomniane rasy podczas tworzenia postaci.\nWymaga ponownego wczytania gry.\n\nDomyślnie: Wł.\n\n",
